01f80a5c42fa2bc39ea5f9ebfbc745813e688dae
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-05-21 14:40:20 +0530

    Stopped displaying usage

--- head.js
+++ head.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const { headMain } = require('./src/headLib.js');
 
-console.log('usage: head [-n lines | -c bytes] [file ...]');
-
 const args = process.argv.slice(2);
 console.log(headMain(fs.readFileSync, ...args));
