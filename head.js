c5729298b4cfaa4e24f391ae9ddaa283e73f9fdc
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-05-24 09:39:15 +0530

    Extracted code of 'head' into main function

--- head.js
+++ head.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const { headMain } = require('./src/headLib.js');
 
-const args = process.argv.slice(2);
-try {
-  headMain(fs.readFileSync, console.log, console.error, ...args);
-} catch (error) {
-  console.error(error.message);
-}
+const main = () => {
+  const args = process.argv.slice(2);
+  try {
+    headMain(fs.readFileSync, console.log, console.error, ...args);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+main();
